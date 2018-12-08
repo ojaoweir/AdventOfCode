115306b4aa80f5a3d0cc7fde824b5162d6efd313
Isak Jansson <isaja339@student.liu.se>
2018-12-08 17:17:48 +0100

    started a little bit on day 6

---
+++ Dec6/largestArea.cs
@@ -0,0 +1,41 @@
+using System;
+
+namespace area
+{
+    class coordinate
+    {
+        int x;
+        int y;
+        public coordinate(int xIn, int yIn)
+        {
+            x = xIn;
+            y = yIn;
+        }
+
+        public int getX()
+        {
+            return x;
+        }
+
+        public int getY()
+        {
+            return y;
+        }
+    }
+    class largestArea
+    {
+        static void Main()
+        {
+            //get all input, one for each row
+            //translate them to coordinates
+                    //coord = translate typ
+            //Make huge double array
+            //place coordinates in the array
+            //for each in array find closest
+                //if equal distance to two - it is nothing
+            //count how many there are of each (so you get the area)
+                //count out those that have their value on the border
+
+        }
+    }
+}
